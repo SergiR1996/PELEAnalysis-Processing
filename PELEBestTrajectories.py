de11d32151022011fd946ff67aa8ce37927c351a
Sergi <sergi.roda@e-campus.uab.cat>
2019-03-20 18:53:36 +0100

    Updated PELEBestTrajectories script

--- PELEBestTrajectories.py
+++ PELEBestTrajectories.py
@@ -58,6 +58,10 @@ def parseArgs():
     -------
     reports : string
               list of report files to look for data
+    energy : float
+              Cutoff of the binding energy
+    sasa :  float
+              Cutoff the SASA parameter
     output_path : string
                   output directory where the resulting plot will be saved
     """
@@ -69,17 +73,22 @@ def parseArgs():
                           type=str, nargs='*', help="path to report files")
     optional.add_argument("-o", "--output", metavar="PATH", type=str,
                           help="output path to save figure", default="Important_trajectories")
+    optional.add_argument("-E", "--energy", metavar="METRIC", type=float,
+                          help="Cutoff of the binding energy", default=-50.0)
+    optional.add_argument("-S", "--sasa", metavar="METRIC", type=float,
+                          help="Cutoff of the SASA parameter", default=0.3)
     parser._action_groups.append(optional)
     args = parser.parse_args()
 
     reports = parseReports(args.input, parser)
 
     output_path = args.output
+    energy,sasa = args.energy,args.sasa
 
-    return reports, output_path
+    return reports, energy, sasa, output_path
 
 
-def Storebesttrajectories(reports):
+def Storebesttrajectories(reports,energy=-50.0,sasa=0.3):
     """It looks on the report files and finds the best trajectories (the minima).
 
     RETURNS
@@ -94,35 +103,45 @@ def Storebesttrajectories(reports):
     Below50={}
     Below55={}
     Below60={}
+    Sasa03={}
     for report in reports:
         reportID=str(os.path.basename(report).split('_')[-1].split('.')[0])
         with open(report, 'r') as report_file:
             next(report_file)
-	    for i, line in enumerate(report_file):
-	        if float(line.split()[4])<=float(-50):
-		    if round(float(line.split()[5]))%5<=2:
-			Distance=(round(float(line.split()[5]))-(round(float(line.split()[5]))%5))
-		    else:
-		        Distance=(round(float(line.split()[5]))+5-(round(float(line.split()[5]))%5))
-		    if Distance not in Below50:
-			Below50[Distance]=[]
-		    if reportID not in Below50[Distance]:
-			Below50[Distance].append(reportID)
-	            if float(line.split()[4])<=float(-55):
-		        if Distance not in Below55:
-			    Below55[Distance]=[]
-		        if reportID not in Below55[Distance]:
-			    Below55[Distance].append(reportID)
-		        if float(line.split()[4])<=float(-60):
-		            if Distance not in Below60:
-			        Below60[Distance]=[]
-		            if reportID not in Below60[Distance]:
-			        Below60[Distance].append(reportID)
+            for i, line in enumerate(report_file):
+                if float(line.split()[4])<=energy:
+                    if round(float(line.split()[5]))%5<=2:
+                        Distance=(round(float(line.split()[5]))-(round(float(line.split()[5]))%5))
+                    else:
+                        Distance=(round(float(line.split()[5]))+5-(round(float(line.split()[5]))%5))
+                    if Distance not in Below50:
+                        Below50[Distance]=[]
+                    if reportID not in Below50[Distance]:
+                        Below50[Distance].append(reportID)
+                    if float(line.split()[4])<=(energy-5.0):
+                        if Distance not in Below55:
+                            Below55[Distance]=[]
+                        if reportID not in Below55[Distance]:
+                            Below55[Distance].append(reportID)
+                        if float(line.split()[4])<=(energy-10.0):
+                            if Distance not in Below60:
+                                Below60[Distance]=[]
+                            if reportID not in Below60[Distance]:
+                                Below60[Distance].append(reportID)
+                if float(line.split()[len(line.split())-1])<=sasa:
+                    if round(float(line.split()[5]))%5<=2:
+                        DistSASA=(round(float(line.split()[5]))-(round(float(line.split()[5]))%5))
+                    else:
+                        DistSASA=(round(float(line.split()[5]))+5-(round(float(line.split()[5]))%5))
+                    if DistSASA not in Sasa03:
+                        Sasa03[DistSASA]=[]
+                    if reportID not in Sasa03[DistSASA]:
+                        Sasa03[DistSASA].append(reportID)
 
-    return Below50,Below55,Below60
+    return Below50,Below55,Below60,Sasa03
 
 
-def Reportbesttrajectories(Below50,Below55,Below60,output_path="Important_trajectories"):
+def Reportbesttrajectories(Below50,Below55,Below60,Sasa03,energy=-50.0,sasa=0.3,output_path="Important_trajectories"):
     """It looks on the report files and finds the best trajectories (the minima).
 
     RETURNS
@@ -131,15 +150,18 @@ def Reportbesttrajectories(Below50,Below55,Below60,output_path="Important_trajec
 		  The report output file with the best trajectories.
     """
     Report=open(output_path,"wt")
-    Report.write("--- Below -50 kcal/mol ---\n")
+    Report.write("--- Below %s kcal/mol ---\n" %energy)
     for key,values in Below50.items():
-	Report.write("%s Ang: " %key + ", ".join(values)+"\n")
-    Report.write("--- Below -55 kcal/mol ---\n")
+        Report.write("%s Ang: " %key + ", ".join(values)+"\n")
+    Report.write("--- Below %s kcal/mol ---\n" %(energy-5.0))
     for key,values in Below55.items():
-	Report.write("%s Ang: " %key + ", ".join(values)+"\n")
-    Report.write("--- Below -60 kcal/mol ---\n")
+        Report.write("%s Ang: " %key + ", ".join(values)+"\n")
+    Report.write("--- Below %s kcal/mol ---\n" %(energy-10.0))
     for key,values in Below60.items():
-	Report.write("%s Ang: " %key + ", ".join(values)+"\n")
+        Report.write("%s Ang: " %key + ", ".join(values)+"\n")
+    Report.write("--- SASA below %s ---\n" %sasa)
+    for key,values in Sasa03.items():
+        Report.write("%s Ang: " %key + ", ".join(values)+"\n")
     Report.close()
 
 
@@ -150,13 +172,13 @@ def main():
     """
 
     # Parse command-line arguments
-    reports, output_path = parseArgs()
+    reports, energy, sasa, output_path = parseArgs()
 
     # Store the best trajectories
-    Report_data_1,Report_data_2,Report_data_3=Storebesttrajectories(reports)
+    Report_data_1,Report_data_2,Report_data_3,Report_data_4=Storebesttrajectories(reports,energy,sasa)
 
     # Generate the report file with the best trajectories
-    Reportbesttrajectories(Report_data_1,Report_data_2,Report_data_3,output_path)
+    Reportbesttrajectories(Report_data_1,Report_data_2,Report_data_3,Report_data_4,energy,sasa,output_path)
 
 
 if __name__ == "__main__":
