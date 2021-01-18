d0e4ec3818e35a0dd260c5bf3710f294be392b0a
Sergi <sergi.roda@e-campus.uab.cat>
2021-01-18 15:40:23 +0100

    ExtractPELEPoseFromTrajectory and PELEAddMetric Python scripts from the PELE_scripts folder were updated to accept trajectory files with the xtc format.

--- PELEAnalysis-Processing/PELE_scripts/ExtractPELEPoseFromTrajectory.py
+++ PELEAnalysis-Processing/PELE_scripts/ExtractPELEPoseFromTrajectory.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 import os
 import argparse as ap
+import mdtraj as md
 
 # Script information
 __author__ = "Sergi Rodà"
@@ -40,42 +41,59 @@ def parseArgs():
                           help="output path to save the pose", default="traj")
     optional.add_argument("-PN", "--particular_name",
                           help="specify if just the string given in the output flag will be used as the output filename", action="store_true")    
+    optional.add_argument("-T", "--topology", metavar="FILE", type=str,
+                          help="Topology file for the PELE trajectory in case it is in xtc format")
     parser._action_groups.append(optional)
     args = parser.parse_args()
 
-    return args.input, args.number, args.second_number, args.output, args.particular_name
+    return args.input, args.number, args.second_number, args.output, args.particular_name, args.topology
 
-def ExtractPoseFromTrajectory(trajectory, number, second_number, output, particular_name):
-	"""
-	This function extracts the lines of the trajectory file that
-	refer to the desired accepted PELE step/s and saves it in an
-	output file.
+def ExtractPoseFromTrajectory(trajectory, number, second_number, output, particular_name, topology):
+    """
+    This function extracts the lines of the trajectory file that
+    refer to the desired accepted PELE step/s and saves it in an
+    output file.
 
-	OUTPUT
-	------
-	The PDB file with the accepted PELE step/s
-	"""
-	write_lines_down = False
-	
-	if particular_name:
-		output_file = open("{}.pdb".format(output),"w")
-	else:
-		if second_number==0:
-			output_file = open("{}_{}_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number),"w")
-		else:
-			output_file = open("{}_{}_from_{}_to_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number,second_number),"w")
+    OUTPUT
+    ------
+    The PDB file with the accepted PELE step/s
+    """
 
-	with open(trajectory, 'r') as traj_file:
-		for line in traj_file:
-			if line[0:5]=="MODEL":
-				if int(line.split()[1])==number:
-					write_lines_down = True
-				elif int(line.split()[1])==number+1 and second_number==0:
-					write_lines_down = False
-				elif int(line.split()[1])==second_number+1:
-					write_lines_down = False
-			if write_lines_down == True:
-				output_file.write(line)
+    if ".xtc" in trajectory:
+        traj = md.load_xtc(trajectory, topology)
+        if second_number==0:
+            if particular_name:
+                traj[number-1].save_pdb("{}.pdb".format(output))
+            else:
+                traj[number-1].save_pdb("{}_{}_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number))
+        else:
+            if particular_name:
+                traj[number-1:second_number].save_pdb("{}.pdb".format(output))
+            else:
+                traj[number-1:second_number].save_pdb("{}_{}_from_{}_to_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number,second_number))
+
+    else:
+        write_lines_down = False
+
+        if particular_name:
+            output_file = open("{}.pdb".format(output),"w")
+        else:
+            if second_number==0:
+                output_file = open("{}_{}_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number),"w")
+            else:
+                output_file = open("{}_{}_from_{}_to_{}.pdb".format(output,trajectory.split("/")[-1].split("_")[-1].split(".")[0],number,second_number),"w")
+
+        with open(trajectory, 'r') as traj_file:
+            for line in traj_file:
+                if line[0:5]=="MODEL":
+                    if int(line.split()[1])==number:
+                        write_lines_down = True
+                    elif int(line.split()[1])==number+1 and second_number==0:
+                        write_lines_down = False
+                    elif int(line.split()[1])==second_number+1:
+                        write_lines_down = False
+                if write_lines_down == True:
+                    output_file.write(line)
 
 def main():
     """
@@ -85,7 +103,7 @@ def main():
     """
 
     # Parse command-line arguments
-    trajectory, number, second_number, output, particular_name = parseArgs()
+    trajectory, number, second_number, output, particular_name, topology = parseArgs()
 
     if second_number!=0 and second_number < number:
     	print("Remember that to make a chunk of the trajectory, the second or last number must be logically higher than the first given number (refering to the first accepted PELE step)")
@@ -93,7 +111,7 @@ def main():
     	exit()
 
     # Extract the accepted PELE step/s from the trajectory file
-    ExtractPoseFromTrajectory(trajectory, number, second_number, output, particular_name)
+    ExtractPoseFromTrajectory(trajectory, number, second_number, output, particular_name, topology)
 
 
 if __name__ == "__main__":
--- PELEAnalysis-Processing/PELE_scripts/PELEAddMetric.py
+++ PELEAnalysis-Processing/PELE_scripts/PELEAddMetric.py
@@ -6,8 +6,7 @@ import os,re
 import glob
 import math
 import argparse as ap
-import pandas as pd
-import numpy as n
+import mdtraj as md
 
 # Local imports
 from PELEParseReports import *
@@ -49,14 +48,14 @@ def parseArgs():
                           help="Format of the report file", default="*out")
     optional.add_argument("-TF","--trajectory_format", metavar="STRING",type=str,
                           help="Format of the trajectory file", default="*pdb")
+    optional.add_argument("-T", "--topology", metavar="FILE", type=str,
+                          help="Topology file for the PELE trajectory in case it is in xtc format")
     parser._action_groups.append(optional)
     args = parser.parse_args()
 
-    out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format = args.input, args.residue, args.metric, args.res_name, args.chain_name, args.column_name, args.report_format, args.trajectory_format
+    return args.input, args.residue, args.metric, args.res_name, args.chain_name, args.column_name, args.report_format, args.trajectory_format, args.topology
 
-    return out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format
-
-def AddMetric(out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format):
+def AddMetric(out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format, topology):
     """
     Take the PELE simulation trajectory files and returns the report files with the desired metric
 
@@ -83,33 +82,42 @@ def AddMetric(out_directory, residue, metric, res_name, chain_name, column_name,
     metric_list = []
 
     for trajectory in trajectory_list:
-        with open(trajectory, 'r') as traj_file:
-            m_list,i = [],0
-            for line in traj_file:
+        if ".xtc" in trajectory:
+            traj = md.load_xtc(trajectory, topology)
+            Atom_pair_1 = int(traj.topology.select("resSeq {} and name {} and resname {} and chainid {}".format(residue[0],metric[0].strip("_"),res_name[0],chain_name[0])))
+            Atom_pair_2 = int(traj.topology.select("resSeq {} and name {} and resname {} and chainid {}".format(residue[1],metric[1].strip("_"),res_name[1],chain_name[1])))
+            metric_list.append(10*md.compute_distances(traj,[[Atom_pair_1,Atom_pair_2]]))
+        else:
+            with open(trajectory, 'r') as traj_file:
+                m_list,i = [],0
+                for line in traj_file:
 
-                if (line[0:4] == "ATOM" or line[0:6] == "HETATM") and (line[22:26].strip() == residue[0] and line[12:16].replace(" ","_") == metric[0] and line[17:20] == res_name[0] and line[21:22] == chain_name[0]) or (line[22:26].strip() == residue[1] and line[12:16].replace(" ","_") == metric[1] and line[17:20] == res_name[1] and line[21:22] == chain_name[1]):
+                    if (line[0:4] == "ATOM" or line[0:6] == "HETATM") and (line[22:26].strip() == residue[0] and line[12:16].replace(" ","_") == metric[0] and line[17:20] == res_name[0] and line[21:22] == chain_name[0]) or (line[22:26].strip() == residue[1] and line[12:16].replace(" ","_") == metric[1] and line[17:20] == res_name[1] and line[21:22] == chain_name[1]):
 
-                    x = float(line[30:38].strip())
-                    y = float(line[38:46].strip())
-                    z = float(line[46:54].strip())
+                        x = float(line[30:38].strip())
+                        y = float(line[38:46].strip())
+                        z = float(line[46:54].strip())
 
-                    if i==0:
-                        first_elem = [x,y,z]
-                        i+=1
-                    else:
-                        second_elem = [x,y,z]
-                        m_list.append(ComputeDistance(first_elem,second_elem))
-                        i=0
+                        if i==0:
+                            first_elem = [x,y,z]
+                            i+=1
+                        else:
+                            second_elem = [x,y,z]
+                            m_list.append(ComputeDistance(first_elem,second_elem))
+                            i=0
 
-            metric_list.append(m_list)
+                metric_list.append(m_list)
 
     for report in report_list:
+        print(report)
         #print(report)
 
         if 'j' not in locals():
             j=0
         else:
             j+=1
+
+        print(j)
 
         with open(report, 'r') as report_file:
             if report.endswith(".out"):
@@ -120,9 +128,9 @@ def AddMetric(out_directory, residue, metric, res_name, chain_name, column_name,
                 if i==0:
                     out_report.write(line.strip("\n")+'    '+column_name+"\n")
                 else:
-                    out_report.write(line.strip("\n")+'    '+str(metric_list[j][i-1])+"\n")
+                    out_report.write(line.strip("\n")+'    '+str(metric_list[j][i-1][0])+"\n")
     
-    print("%s report files have the desired metric added"%j)
+    print("{} report files have the desired metric added".format(j+1))
 
 def main():
     """
@@ -132,10 +140,10 @@ def main():
     """
 
     # Parse command-line arguments
-    out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format  = parseArgs()
+    out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format, topology  = parseArgs()
 
     # Add the desired metric to the report file
-    AddMetric(out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format)
+    AddMetric(out_directory, residue, metric, res_name, chain_name, column_name, report_format, trajectory_format, topology)
 
 
 if __name__ == "__main__":
